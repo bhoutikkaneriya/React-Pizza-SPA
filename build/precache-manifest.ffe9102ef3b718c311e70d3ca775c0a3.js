c96cd295effefc30ac6fe5de3b304b39cb9c5256
Bhoutik <kaneriya.bhoutik87@gmail.com>
2019-10-28 12:23:39 -0700

    Implmented UI and order functionality

---
+++ build/precache-manifest.ffe9102ef3b718c311e70d3ca775c0a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eafb53d0de6cba66a82798b977ab5a59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54644f9e435b6f54cc45",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "563a3b87baca65c27e55",
+    "url": "/static/js/2.d8ee710b.chunk.js"
+  },
+  {
+    "revision": "54644f9e435b6f54cc45",
+    "url": "/static/js/main.d5594b31.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
